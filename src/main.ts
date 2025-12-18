6a9a50e6f61a65c7e1f29869638579b6f7232885
Ping <muhammadyusufa2111@gmail.com>
2025-12-18 16:51:53 +0500

    fixed deployment to vercel

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import { join } from 'path';
 
-import './schedule.keep-alive';
+import './schedule.keep-alive'; // оставляем, если нужен
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init(); // важно для Serverless
 }
+
 bootstrap();
+
+export default server;
